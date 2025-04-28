5e96673139846298ff6c2825a754baaf63dd27b4
laura <lauravtd44@gmail.com>
2025-04-28 09:49:15 -0400

    more methods

--- Athlete.java
+++ Athlete.java
@@ -11,14 +11,16 @@ public class Athlete
     private String name;               // Name of the athlete
     private Gender gender;             // Gender of the athlete
     private ArrayList<Activity> activities;  // List of activities the athlete has performed
-    private double weight;             // Athlete's weight (used for calorie calculations)
-    private int dateOfBirth; 
+    private double weight;// Athlete's weight (used for calorie calculations)
+    private double height;
+    private int yearOfBirth; 
 
-    public Athlete(String name, Gender gender, double weight, int dateOfBirth) {
+    public Athlete(String name, Gender gender, double weight,double height, int yearOfBirth) {
         this.name = name;
         this.gender = gender;
         this.weight = weight;
-        this.dateOfBirth = dateOfBirth;
+        this.height = height;
+        this.yearOfBirth = yearOfBirth;
         this.activities = new ArrayList<>();
     }
     
@@ -26,5 +28,16 @@ public class Athlete
      return this.name;
     }
     
-
+    public void addActivity(Activity activity){
+      this.activities.add(activity);  
+    }
+    
+    public void athleteInfo()
+    {
+        System.out.println("Name: ");
+        System.out.println("Gender: ");
+        System.out.println("Height: "+height+" Weight: "+weight);
+        System.out.println("Year of birth: "+ yearOfBirth);
+        System.out.println("My activities: "+activities);
+    }
 }
