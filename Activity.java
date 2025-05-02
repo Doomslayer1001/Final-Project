6822e0aab9406ad902d2d1ed3a1a1ec15adc9630
Danush Soosai <danushsoosai28@gmail.com>
2025-05-02 09:35:27 -0400

    Added Equipment Item to Activity also added weight to Equipment

--- Activity.java
+++ Activity.java
@@ -12,13 +12,19 @@ public class Activity
     private String description;        // Description of the activity
     private Modality modality;         
     private double duration;  
-    
     public Activity(String name, double distance, String description, Modality modality, double duration) {
         this.name = name;
         this.distance = distance;
         this.description = description;
         this.modality = modality;
         this.duration = duration;   
+        
+    }
+    
+    private void Equipment(){
+        Equipment bike = new Equipment("Bike", 1.5);
+        Equipment skate = new Equipment("Skate",4.5);
+        Equipment poll = new Equipment("Poll (depending on the weight of the person)", 0);
     }
     
     public String getName(){
@@ -27,7 +33,6 @@ public class Activity
     
     public double getDistance()
     {
-         
        return this.distance;
     }
     
@@ -45,8 +50,7 @@ public class Activity
     
     public Modality getModality()
     {
-        
-      return this.modality;
+        return this.modality;
     }
     
 }
