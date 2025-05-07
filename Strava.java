2401a29a3b13d8c976dee0812eb7fd172251e16e
Danush <danushsoosai28@gmail.com>
2025-05-06 22:38:12 -0400

    Deleted Menu readded scanner to Stava class

--- Strava.java
+++ Strava.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-
+import java.util.Scanner;
 /**
  * Write a description of class Strava here.
  *
@@ -10,13 +10,22 @@ public class Strava
 {
     private HashMap<Integer,String> athletes;
     private int idAthletes=0;
-    private Menu menu;
     public Strava()
     {
         athletes = new HashMap<>();
-        menu = new Menu();
     }
-   
+    
+    public void addAthletes(){
+        System.out.println("---Menu---");
+        Scanner scan =new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scan.next();
+        System.out.println("What is your last name?");
+        String last_name = scan.next();
+        Choice();
+        //athletes.put(athlete.getId(),athlete.getName());   
+    }
+    
     public void listAllAthletes()
     {
         System.out.println("Found athletes are: "); 
@@ -24,6 +33,37 @@ public class Strava
         for (Integer id: athletes.keySet()){
             System.out.println("Id: " +id + " Name: "+ athletes.get(id));
         }
+    }
+       private Equipment Choice()
+    {
+        Equipment bike = new Equipment("Bike", 1.5);
+        Equipment skate = new Equipment("Skate",4.5);
+        Equipment poll = new Equipment("Poll (depending on the weight of the person)", 0);
+        Scanner scan =new Scanner(System.in);
+        Equipment chosen = null;
+            while(chosen == null){
+            System.out.println("Choose an equipment");
+            System.out.println("1. Bike");
+            System.out.println("2. Skate");
+            System.out.println("3. Poll");
+            System.out.println("please choose an equipment");
+            int value = scan.nextInt();
+            if (value == 1 ){
+            chosen = bike;
+            }
+            else if( value == 2){
+            chosen = skate;
+            }
+            else if (value == 3){
+            chosen = poll;
+            }
+            else
+            {
+            System.out.println("Invalid option try again");
+            }
+        
+        }
+        return chosen;
     }
 
     public int giveId(){
